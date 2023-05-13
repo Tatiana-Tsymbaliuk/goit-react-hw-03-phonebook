79b64b818bdbab8e098de6d3fab3edd87fd0c3fc
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-13 16:25:19 +0300

    crete old project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+
+import React from 'react';
+import { nanoid } from 'nanoid'
+import ContactForm from 'components/FormPhonebook/ContactForm'
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+export class App extends React.Component{
+  state = {
+    contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: '',
+    }  
+    
+    handleSubmit = (data) => {
+      const sameName = this.state.contacts
+        .map((cont) => cont.name)
+        .includes(data.name);
+      if (sameName) {
+        alert(`${data.name} is already in contacts`);
+      } else {
+        this.setState((prevState) => ({
+          contacts: [...prevState.contacts, {id: nanoid(), ...data }],
+        }));
+      }
+    };
+    changeFilter =(e)=>{
+      this.setState({filter:e.currentTarget.value})
+    }
+    getVisibleContats =()=>{
+      const {contacts, filter} = this.state
+      console.log(this.state); 
+      const normalizedFilter = filter.toLowerCase();
+      return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    }
+    deleteTodo = todoId =>{
+      this.setState(prevState =>({
+      contacts: prevState.contacts.filter(contact=>contact.id !== todoId),
+    }));}
+
+    render(){
+    const {filter} = this.state
+    const visibleContacts = this.getVisibleContats()
+
+    return (<div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmitForm={this.handleSubmit}/>
+      <h2>Contacts</h2> 
+      <Filter value={filter} onChangeFilter={this.changeFilter}/>
+      <ContactList contacts={visibleContacts} onDeleteTodo={this.deleteTodo}/>
+    </div>) 
+    }    
+}
+
