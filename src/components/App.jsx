5466a4f50cd87fe5a5f191448f3a87f52fcba980
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-19 19:45:18 +0300

    corected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import Filter from 'components/Filter/Filter';
 export class App extends React.Component{
   state = {
     contacts: [
-          {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-          {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-          {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-          {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}, 
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}, 
   ],
     filter: '',
     }  
@@ -42,25 +42,19 @@ export class App extends React.Component{
       contacts: prevState.contacts.filter(contact=>contact.id !== todoId),
     }));}
     componentDidMount(){
-      console.log('App componentDidMount');
       const contacts = localStorage.getItem('contacts');
       const parsedContacts = JSON.parse(contacts);
-      console.log(parsedContacts);
+      if (parsedContacts){
       this.setState({contacts: parsedContacts });
-    }
+    }}
 
     componentDidUpdate(prevProps, prevState){
-      console.log('App componentDidUpdate');
-      console.log(prevState);
-      console.log(this.state);
       if(this.state.contacts !==prevState.contacts){
-        console.log('Обновилось поле contacts');
         localStorage.setItem('contacts', JSON.stringify(this.state.contacts) );
       }
     }
 
     render(){
-      console.log('App render');
     const {filter} = this.state
     const visibleContacts = this.getVisibleContats()
 
